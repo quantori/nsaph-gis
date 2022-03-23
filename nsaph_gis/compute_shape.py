601ffee6aaef9e7447dd735135bdfb77a8ba6989
Mikhail Polykovskiy <mikhail.polykovskiy@quantori.com>
2022-03-23 21:53:33 +0400

    Compute shapes

--- nsaph_gis/compute_shape.py
+++ nsaph_gis/compute_shape.py
@@ -1,97 +1,93 @@
-from constants import RasterizationStrategy, Geography
+from dataclasses import dataclass
+from typing import Iterable, Optional
+
+import rasterio
+from rasterstats import zonal_stats
+
+from .constants import RasterizationStrategy
+
+NO_DATA = -999  # I do not know what it is, but not setting it causes a warning
 
 
-class ComputeShapesTask:
-    """
-    Class describes a compute task to aggregate data over geography shapes
-
-    The data is expected in
-    .. _Unidata netCDF (Version 4) format: https://www.unidata.ucar.edu/software/netcdf/
-    """
-
-    def __init__(
-            self, year: int, variable: GridmetVariable, infile: str,
-            outfile: str, strategy: RasterizationStrategy, shapefile: str,
-            geography: Geography, date_filter=None
-    ) -> None:
-        """
-        :param date_filter:
-        :param year: year
-        :param variable: Gridemt band (variable)
-        :param infile: File with source data in  NCDF4 format
-        :param outfile: Resulting CSV file
-        :param strategy: Rasterization strategy to use
-        :param shapefile: Shapefile for used collection of geographies
-        :param geography: Type of geography, e.g. zip code or county
-        """
-
-        super().__init__(year, variable, infile, outfile, date_filter)
+@dataclass
+class Record:
+    mean: Optional[float]
+    prop: str
 
 
-        if strategy == RasterizationStrategy.downscale:
-            self.strategy = RasterizationStrategy.default
-            self.factor = 5
-        else:
-            self.strategy = strategy
-        self.shapefile = shapefile
-        self.geography = geography
+class StatsCounter:
+    @classmethod
+    def process(
+        cls,
+        strategy: RasterizationStrategy,
+        shapefile: str,
+        affine: rasterio.Affine,
+        layer: Iterable
+    ) -> Iterable[Record]:
+        all_touched_strategies = [
+            RasterizationStrategy.default,
+            RasterizationStrategy.combined,
+            RasterizationStrategy.downscale,
+        ]
+        non_all_touched_strategies = [
+            RasterizationStrategy.all_touched,
+            RasterizationStrategy.combined,
+        ]
 
-    def to_date(self, day) -> datetime.date:
-        origin = date(1900, 1, 1)
-        return self.origin + timedelta(days=day)
+        stats = []
+        if strategy in all_touched_strategies:
+            stats.append(
+                zonal_stats(
+                    shapefile,
+                    layer,
+                    stats="mean",
+                    affine=affine,
+                    geojson_out=True,
+                    all_touched=False,
+                    nodata=NO_DATA,
+                )
+            )
+        if strategy in non_all_touched_strategies:
+            stats.append(
+                zonal_stats(
+                    shapefile,
+                    layer,
+                    stats="mean",
+                    affine=affine,
+                    geojson_out=True,
+                    all_touched=True,
+                    nodata=NO_DATA,
+                )
+            )
 
-    def compute_one_day(self, writer: Collector, day, layer):
-        dt = self.to_date(day)
-        if self.factor > 1:
-            layer = disaggregate(layer, self.factor)
-        logging.info("{}:{}:{}".format(
-            self.geography.value,self.band.value,str(dt))
-        )
-        l = None
-        stats1 = []
-        stats2 = []
-        if self.strategy in [RasterizationStrategy.default,
-                             RasterizationStrategy.combined]:
-            stats1 = zonal_stats(self.shapefile, layer, stats="mean",
-                                 affine=self.affine, geojson_out=True,
-                                 all_touched=False, nodata=NO_DATA)
-            l = len(stats1)
-        if self.strategy in [RasterizationStrategy.all_touched,
-                             RasterizationStrategy.combined]:
-            stats2 = zonal_stats(self.shapefile, layer, stats="mean",
-                                 affine=self.affine, geojson_out=True,
-                                 all_touched=True, nodata=NO_DATA)
-            l = len(stats2)
+        if not len(stats[0]):
+            return
 
-        for i in range(0, l):
-            record = stats1[0] if stats1 else stats2[0]
-            key = self.determine_key(record)
+        row = stats[0][0]
+        key = cls._determine_key(row)
 
-            if self.strategy == RasterizationStrategy.combined:
-                mean, prop = self.combine(key, stats1[i], stats2[i])
-            elif self.strategy == RasterizationStrategy.default:
-                mean = stats1[i]['properties']['mean']
-                prop = stats1[i]['properties'][key]
+        for i in range(len(stats[0])):
+            if len(stats) == 2:
+                # Combined strategy
+                mean, prop = cls._combine(key, stats[0][i], stats[1][i])
+
             else:
-                mean = stats2[i]['properties']['mean']
-                prop = stats2[i]['properties'][key]
-            writer.writerow([mean, dt.strftime("%Y-%m-%d"), prop])
-        return
+                mean = stats[0][i]['properties']['mean']
+                prop = stats[0][i]['properties'][key]
 
-    def get_key(self):
-        return self.geography.value.upper()
+            yield Record(mean=mean, prop=prop)
 
     @staticmethod
-    def determine_key(record) -> str:
-        if "ZIP" in record['properties']:
+    def _determine_key(row) -> str:
+        if "ZIP" in row['properties']:
             return "ZIP"
-        elif "ZCTA5CE10" in record['properties']:
+        elif "ZCTA5CE10" in row['properties']:
             return "ZCTA5CE10"
         else:
             raise ValueError("Unknown shape format, no ZIP neither ZCTA5CE10")
 
     @staticmethod
-    def combine(key, r1, r2):
+    def _combine(key, r1, r2) -> Record:
         prop = r1['properties'][key]
         assert prop == r2['properties'][key]
 
@@ -105,4 +101,4 @@ class ComputeShapesTask:
             raise AssertionError("m1 && !m2")
         else:
             mean = None
-        return mean, prop
+        return Record(mean=mean, prop=prop)
